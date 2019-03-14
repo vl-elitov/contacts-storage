c4e7007d7a026352c8cf1d301b51244b170751c8
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-03-14 15:08:08 +0300

    Contacts component created

---
+++ resources/js/components/ContactsComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Contacts</h1>
+</template>
+
+<script>
+    export default {
+        name: "contacts",
+        data: function () {
+            return {}
+        },
+        mounted() {
+            console.log('Contacts Component Loaded...');
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
