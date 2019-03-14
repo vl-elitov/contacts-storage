85a065159601bc72f7edc65cd621c7f26c3d646f
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-03-14 14:38:19 +0300

    Routes my api and added phpdoc

--- app/Contact.php
+++ app/Contact.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Contact
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact query()
+ * @mixin \Eloquent
+ */
 class Contact extends Model
 {
     //
